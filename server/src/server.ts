1763ccacbcd5b62c17e045b88eb6813174ee0413
tim <ltiancong@gmail.com>
2024-06-14 15:07:28 +1000

    Build basic express server

---
+++ server/src/server.ts
@@ -0,0 +1,68 @@
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+interface Issue {
+    id: string;
+    title: string;
+    description: string;
+}
+
+let issues: Issue[] = [];
+
+// Create a new issue
+app.post('/issues', (req: Request, res: Response) => {
+    const issue: Issue = req.body;
+    issues.push(issue);
+    res.status(201).send(issue);
+});
+
+// Get an issue by ID
+app.get('/issues/:id', (req: Request, res: Response) => {
+    const issueId = req.params.id;
+    const issue = issues.find(_issue => _issue.id === issueId);
+    if (issue) {
+        res.status(200).send(issue);
+    } else {
+        res.status(404).send({ message: 'Issue not found' });
+    }
+});
+
+// Update an issue by ID
+app.put('/issues/:id', (req: Request, res: Response) => {
+    const issueId = req.params.id;
+    const issueIndex = issues.findIndex(_issue => _issue.id === issueId);
+    if (issueIndex !== -1) {
+        issues[issueIndex] = req.body;
+        res.status(200).send(issues[issueIndex]);
+    } else {
+        res.status(404).send({ message: 'Issue not found' });
+    }
+});
+
+// Delete an issue by ID
+app.delete('/issues/:id', (req: Request, res: Response) => {
+    const issueId = req.params.id;
+    const issueIndex = issues.findIndex(_issue => _issue.id === issueId);
+    if (issueIndex !== -1) {
+        const deletedIssue = issues.splice(issueIndex, 1);
+        res.status(200).send(deletedIssue);
+    } else {
+        res.status(404).send({ message: 'Issue not found' });
+    }
+});
+
+// For testing, get all issues
+app.get('/issues', (_: Request, res: Response) => {
+    res.status(200).send(issues);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+
