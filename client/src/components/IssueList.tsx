4b267624e91ab6d9d005b51f59e92a2e1745a3ad
tim <ltiancong@gmail.com>
2024-06-14 16:32:18 +1000

    fix wrong indents

--- client/src/components/IssueList.tsx
+++ client/src/components/IssueList.tsx
@@ -3,29 +3,29 @@ import axios from 'axios';
 import { Issue } from '../types';
 
 interface IssueListProps {
-	issues: Issue[];
-	openModal: (issue: Issue) => void;
-	deleteIssue: (id: string) => void;
+  issues: Issue[];
+  openModal: (issue: Issue) => void;
+  deleteIssue: (id: string) => void;
 }
 
 const IssueList: React.FC<IssueListProps> = ({ issues, openModal, deleteIssue }) => {
-	const handleDelete = async (issueId: string) => {
-		await axios.delete(`http://localhost:3001/issues/${issueId}`);
-		deleteIssue(issueId);
-	}
-	return (
-		<div>
-			{issues.map((issue, idx) => (
-				<div className="issue-item" key={idx}>
-					<span>{issue.title}</span>
-					<div>
-						<button className="edit-button" onClick={() => openModal(issue)}>Edit</button>
-						<button onClick={() => handleDelete(issue.id)}>Delete</button>
-					</div>
-				</div>
-			))}
-		</div>
-	);
+  const handleDelete = async (issueId: string) => {
+    await axios.delete(`http://localhost:3001/issues/${issueId}`);
+    deleteIssue(issueId);
+  }
+  return (
+    <div>
+      {issues.map((issue, idx) => (
+        <div className="issue-item" key={idx}>
+          <span>{issue.title}</span>
+          <div>
+            <button className="edit-button" onClick={() => openModal(issue)}>Edit</button>
+            <button onClick={() => handleDelete(issue.id)}>Delete</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default IssueList;
